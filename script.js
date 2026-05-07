50e34943afc34a6e7c9ba7808d01d8209dda7bb6
Erick Ochieng <eoochieng044@gmail.com>
2026-05-07 05:34:29 +0300

    made changes on the body style

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ window.addEventListener("load", function() {
 "(Starting with sunday at index 0)"
 const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 const femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",];
+
+
 document.getElementById('akan-form').addEventListener('submit',function(event) {
     event.preventDefault();
 
@@ -13,8 +15,8 @@ document.getElementById('akan-form').addEventListener('submit',function(event) {
     const year = Number(document.getElementById('year').value);
     const gender = document.getElementById('gender').value;
 
-    // validation
-    
+// validation
+
     if (DD < 1 || DD > 31){
         alert("Enter a valid day")
         return;
@@ -30,25 +32,35 @@ document.getElementById('akan-form').addEventListener('submit',function(event) {
         return;
     }
 
-    const CC = parseInt(year.toString().slice(0,2));
-    const YY = parseInt(year.toString().slice(2,4));
+// Gettin the year
 
+    const CC = Number(year.toString().slice(0,2));
+    const YY =  Number(year.toString().slice(2,4));
+
+// formula
     let d = ( (Math.floor(CC/4) -(2*CC)-1)+(Math.floor(5*YY/4) )+(Math.floor(26*(MM+1)/10) ) + DD ) % 7;
 
     let dayOfWeek = Math.floor(d);
 
     if (dayOfWeek < 0) {
-        dayOfWeek = dayOfWeek + 7;
+        dayOfWeek += 7;
     }
+
+
+// Akan name
     let akanName = (gender ==="male") ? maleNames[dayOfWeek] :femaleNames[dayOfWeek];
-    const resultDisplay = document.getElementById('display-text');
+
+// Results
+    const resultDisplay = document.getElementById('results');
+
     resultDisplay.innerText = "Your Akan name is" + akanName + "!";
+
     resultDisplay.style.color = "blue";
 });
 
 
-function getDayName(dayIndex) {
-    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    return days[dayIndex];
-}
+// function getDayName(dayIndex) {
+//     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//     return days[dayIndex];
+// }
 
