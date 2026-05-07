af20345e25b92c586a2aaabbbbbe7797319fd91a
Erick Ochieng <eoochieng044@gmail.com>
2026-05-07 05:12:25 +0300

    made change assigning values

--- script.js
+++ script.js
@@ -1,37 +1,48 @@
+window.addEventListener("load", function() {
+    console.log("Page fully loaded with all resources!");
+});
 
-
-const maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku","Yaw", "Kofi", "Kwame"];
-const femaleNames = ["Akosua","Adwoa", "Abenaa", "Akua", "Yaa", "Afua","Ama"];
-
-document.getElementById("akanForm").addEventListener("submit", function(event) {
+"(Starting with sunday at index 0)"
+const maleNames =["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+const femaleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama",];
+document.getElementById('akan-form').addEventListener('submit',function(event) {
     event.preventDefault();
 
-const birthdayInput = document.getElementById('birthday').value;
-const genderElement = document.querySelector('input[name="gender"]:checked')
+    // const birthdayInput = document.getElementById('birthday').value;
+    // const genderElement = document.querySelector('input[name="gender"]:checked')
 
-if (!birthdayInput || !genderElement) {
-    alert("Select your birthdate and gender!");
-    return;
-}
+    // if (!birthdayInput || ! genderElement) {
+    //     alert("please select your birthdate andgender!"); return;
+    // }
 
-const gender = genderElement.value;
-const dateObj = new Date(birthdayInput);
-const DD = dateObj.getDate();
-const MM = dateObj.getMonth() +1;
-const year = dateObj.getFullYear();
-const CC = parseInt(year.toString().slice(0,2));
-const YY = parseInt(year.toString().slice(2,4));
+    // const gender =genderElement.value;
+    // const dateObj = new Date(birthdayInput);
+    // const DD = dateObj.getDate();
+    // const MM = dateObj.getMonth() +1;
+    // const year = dateObj.getFullYear();
+    const DD = Number(document.getElementById('day').value);
+    const MM = Number(document.getElementById('month').value);
+    const year = Number(document.getElementById('year').value);
+    const gender = document.getElementById('gender').value;
+    const CC = parseInt(year.toString().slice(0,2));
+    const YY = parseInt(year.toString().slice(2,4));
 
-let d = ( (Math.floor(CC/4) -(2*CC)-1)+(Math.floor(5*YY/4) )+(Math.floor(26*(MM+1)/10) ) + DD ) % 7;
+    let d = ( (Math.floor(CC/4) -(2*CC)-1)+(Math.floor(5*YY/4) )+(Math.floor(26*(MM+1)/10) ) + DD ) % 7;
 
-let dayOfWeek = Math.floor(d);
+    let dayOfWeek = Math.floor(d);
 
-if (dayOfWeek < 0){
-    dayOfWeek = dayOfWeek + 7;
-}
-
-let akanName = (gender === "male") ? maleNames[dayOfWeek] :femaleNames[dayOfWeek];
-const resultDisplay = document.getElementById('display-text');
-resultDisplay.innerText = "Your Akan Name is" + akanName + "!";
-resultDisplay.style.color = "blue";
+    if (dayOfWeek < 0) {
+        dayOfWeek = dayOfWeek + 7;
+    }
+    let akanName = (gender ==="male") ? maleNames[dayOfWeek] :femaleNames[dayOfWeek];
+    const resultDisplay = document.getElementById('display-text');
+    resultDisplay.innerText = "Your Akan name is" + akanName + "!";
+    resultDisplay.style.color = "blue";
 });
+
+
+function getDayName(dayIndex) {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return days[dayIndex];
+}
+
